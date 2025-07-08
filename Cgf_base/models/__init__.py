236a50afbb0640a380d72260ec5acb711f67e985
jolagann <joachim5943@gmail.com>
2025-07-08 10:05:24 +0200

    init

---
+++ Cgf_base/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from custom.mod.models import cgf_class_newspaper
+from custom.mod.models import cgf_base
+from custom.mod.models import cdg_newsletter
+
+
+
---
+++ cgf_class_newspaper/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+from . import cgf_class_newspaper
+
+
+
