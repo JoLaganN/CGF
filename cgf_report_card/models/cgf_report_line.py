9c4f799f829434b48b097aa208dd24c95f1ac7f6
MrSlash <63912929+MSl4sh@users.noreply.github.com>
2025-07-08 11:54:47 +0200

    creating models + methods

--- cgf_report_card/models/cgf_report_line.py
+++ cgf_report_card/models/cgf_report_line.py
@@ -4,7 +4,7 @@ class ReportLine(models.Model):
     _name = 'cgf.report_line'
     _description = 'Report line'
 
-    report_id = fields.Many2one('bulletin.bulletin', required=True, ondelete='cascade')
+    report_id = fields.Many2one('cgf.report', required=True, ondelete='cascade')
     course_name = fields.Char(required=True)
     teacher_name = fields.Char()
     hours = fields.Float(string="Course Hours")
