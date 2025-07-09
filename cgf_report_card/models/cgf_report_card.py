920e2f3daa0cdd5f3c9174abaac6fe77fe3a1252
MrSlash <63912929+MSl4sh@users.noreply.github.com>
2025-07-09 10:27:03 +0200

    merge from develop + fix report id generation

--- cgf_report_card/models/cgf_report_card.py
+++ cgf_report_card/models/cgf_report_card.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import datetime
 
 class Report(models.Model):
     _name = 'cgf.report'
@@ -7,8 +8,8 @@ class Report(models.Model):
 
     sequence = fields.Char(string='Report ID', required=True, copy=False, readonly=True, default='New')
     date = fields.Date(required=True)
-    student_id = fields.Many2one('res.partner', domain=[('is_student', '=', True)], required=True)
-    homeroom_teacher = fields.Char(string="Homeroom Teacher")
+    student_id = fields.Many2one('res.partner', domain=[('type_contact', '=', "student")], required=True)
+    homeroom_teacher = fields.Many2one('res.partner', domain=[('type_contact', '=', "professor")], required=True)
     classroom = fields.Char(string="Class")
     period_id = fields.Many2one('cgf.report_period', required=True)
     line_ids = fields.One2many('cgf.report_line', 'report_id', string="Courses")
@@ -17,8 +18,18 @@ class Report(models.Model):
 
     @api.model
     def create(self, vals):
+        # Si sequence est encore "New", on construit notre identifiant
         if vals.get('sequence', 'New') == 'New':
-            vals['sequence'] = self.env['ir.sequence'].next_by_code('cgf.report') or '/'
+            # Récupération des éléments nécessaires
+            period = self.env['cgf.report_period'].browse(vals.get('period_id'))
+            student = self.env['res.partner'].browse(vals.get('student_id'))
+            date = vals.get('date') or fields.Date.today()
+            year = datetime.strptime(date, '%Y-%m-%d').year if isinstance(date, str) else date.year
+
+            period_name = period.name or 'NoPeriod'
+            student_name = student.name.replace('/', '-') if student else 'NoName'
+
+            vals['sequence'] = f"{year}/{period_name}/{student_name}"
         return super().create(vals)
 
     @api.depends('line_ids.grade', 'line_ids.hours')
